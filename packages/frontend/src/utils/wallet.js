1518e19a788c5c9c2c16c7be6d9e47c41d8b6f3a
esaminu <esaminu1@icloud.com>
2022-06-07 23:05:33 +0400

    fix: lint errors

--- packages/frontend/src/utils/wallet.js
+++ packages/frontend/src/utils/wallet.js
@@ -351,7 +351,7 @@ class Wallet {
                             if (ledgerHdPath) {
                                 setLedgerHdPath({accountId, path: ledgerHdPath});
                             }
-                            return this.getLedgerPublicKey(ledgerHdPath).then((publicKey) => setKeyMeta(publicKey.toString(), {type: "ledger"}));
+                            return this.getLedgerPublicKey(ledgerHdPath).then((publicKey) => setKeyMeta(publicKey.toString(), {type: 'ledger'}));
                         }
                     })
                     .then(() => {
